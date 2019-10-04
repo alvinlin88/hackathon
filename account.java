55448f9aaa1c07e6411926dd8bb7a8911dd359da
Alvin Lin <alvinlin88@gmail.com>
2019-10-04 11:13:59 -0700

    data

---
+++ account.java
@@ -0,0 +1,13 @@
+public class Account {
+		int balance;
+		String username;
+		String name;
+		
+		public Account(int balance, String username, String name){
+			this.balance = balance;
+			this.username = username;
+			this.name = name;
+		}
+		
+		
+}
---
+++ transfer.java
@@ -0,0 +1,9 @@
+public class Transfer implements Account {
+	
+	private ArrayList<Account> list = new ArrayList<Account>
+	
+	public static void main(String args[]){
+		list.add(new super())
+	}
+	
+}
